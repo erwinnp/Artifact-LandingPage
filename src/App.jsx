851e1659d46354da559c0907ffd7870439817408
moonix0 <moongraph21@gmail.com>
2023-02-26 21:41:13 +0700

    Slicing Featured NFTs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navbar } from './components'
 import About from './sections/About'
+import Features from './sections/Features'
 import Hero from './sections/Hero'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Features />
     </>
   )
 }
